e6bb70af407e0e667a02a92d56c5db2cff7f6b07
Markiyan <pacajmarkiyan@gmail.com>
2024-03-21 10:17:37 +0200

    Тепер по айді виводить фільм

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from swapi import swapi
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_film_info():
+    film = swapi.get_film(1)
+    print(f"Фільм: {film.title}")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# hhhh
+    film_id = input("введіть ідентефікатор фільму: ")
+    get_film_info()
