d99b762e9a5866217c610bfdac2cc8d618b916da
Markiyan <pacajmarkiyan@gmail.com>
2024-03-21 11:58:23 +0200

    код працює з Swapi

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from swapi import swapi
+from swapi import get_film
 
 
 def get_film_info():
-    film = swapi.get_film(1)
+    film = get_film(1)
+    characters = film.get_characters().items
+    vehicles = film.get_vehicles().items
+    starships = film.get_starships().items
+    species = film.get_species().items
+
     print(f"Фільм: {film.title}")
 
+    print("Персонажі: ")
+    for index, character in enumerate(characters, 1):
+        print(f"{index} {character.name} з планети ({character.get_homeworld().name})")
 
-if __name__ == '__main__':
-    film_id = input("введіть ідентефікатор фільму: ")
+    print("Транспортні засоби: ")
+    for index, vehicle in enumerate(vehicles, 1):
+        print(f"{index} {vehicle.name}")
+
+    print("Космічні кораблі: ")
+    for index, starship in enumerate(starships, 1):
+        print(f"{index} {starship.name}")
+
+    print("Види істот: ")
+    for index, specie in enumerate(species, 1):
+        print(f"{index} {specie.name}")
+
+
+if __name__ == "__main__":
+    film_id = input("Введіть ідентифікатор фільму: ")
     get_film_info()
